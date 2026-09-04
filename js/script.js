971f86a92550ed65d906bfeadb08c2ef9b83a75e
foreverevolvingservices-svg <forever.evolving.services@gmail.com>
2026-09-04 18:56:24 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 function toggleMenu() {
     const mobileNav = document.getElementById("mobileNav");
+const ambientSound = document.getElementById('ambientSound');
+let soundPlayed = false;
+
+window.addEventListener('scroll', () => {
+  const storySection = document.getElementById('our-story');
+  const rect = storySection.getBoundingClientRect();
+
+  if (!soundPlayed && rect.top < window.innerHeight * 0.75) {
+    ambientSound.volume = 0.25;
+    ambientSound.play();
+    soundPlayed = true;
+  }
+});
 
     if (mobileNav.style.display === "flex") {
         mobileNav.style.display = "none";
